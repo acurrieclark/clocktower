bdbd0b352b7778aef2ba9e73bce766296b973d13
Alex Currie-Clark <acurrieclark@gmail.com>
2015-05-05 23:27:53 +0200

    5.4 compatibility Other upgrades from messenger code

--- library/structures/user.php
+++ library/structures/user.php
@@ -2,8 +2,8 @@
 
 $structure = array(
 			array('Email Address', 'email', 'short_name' => 'email', 'unique' => true, 'unique_message' => "This email has already been used. Please enter a different address."),
-			array("Password", "hashed_password", 'should_be_unique' => true, 'short_name' => "hashed_password"),
-			array("salt", "hashed_password", 'should_be_unique' => true),
+			array("Password", "hashed_password", 'unique' => true, 'short_name' => "hashed_password"),
+			array("salt", "hashed_password", 'unique' => true),
 			array("Level", "drop", array("Administrator", "Editor", "User"), 'short_name' => 'level'),
 			array("session_id", "id_code"),
 			array("remember_token", "id_code"),
